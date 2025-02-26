e769bf73e328e785432e5bbf098b585de0ee0211
Adedayo UWensuyi <onehpee@yahoo.com>
2025-02-25 23:55:15 -0600

    practice test

---
+++ main.py
@@ -0,0 +1,5 @@
+def get_food(temp):
+    if temp > 50:
+        return "rice"
+    else:
+        return "stew"
---
+++ test_main.py
@@ -0,0 +1,4 @@
+from main import get_food
+
+def test_get_food():
+    assert get_food(55) == "rice"
