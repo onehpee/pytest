fe21708ffcfd3c6472bb91ea203f72b32be81a70
Adedayo UWensuyi <onehpee@yahoo.com>
2025-02-26 22:24:12 -0600

    practice test

--- main.py
+++ main.py
@@ -3,3 +3,27 @@ def get_food(temp):
         return "rice"
     else:
         return "stew"
+    
+    
+def add(a, b):
+    return a + b
+
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
+
+class User:
+    def __init__(self):
+        self.users = ()
+        
+    def add_user(self, username, email):
+        if username in self.users:
+            raise ValueError("User already exists")
+        self.users[username] = email
+        return True
+    
+    def get_user(self, username):
+        return self.users.get(username)
--- test_main.py
+++ test_main.py
@@ -1,4 +1,17 @@
-from main import get_food
+from main import get_food, add, divide
+import pytest
 
 def test_get_food():
     assert get_food(55) == "rice"
+    
+    
+def test_add():
+    assert add(9, 1) == 10, "9 + 1 should be 10"
+    assert add(-6, 4) == -2, "-6 + 4 should be -2"
+    assert add(0, 0) == 0, "0 + 0 should be 0"
+    
+    
+def test_divide():
+    with pytest.raises(ValueError, match="Cannot divide by zero"):
+        divide(4, 0)
+    
