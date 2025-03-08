1628164edf4152e87cd817406921ff0ce1c24d86
Adedayo UWensuyi <onehpee@yahoo.com>
2025-03-07 23:54:43 -0600

    practice test

--- test_db.py
+++ test_db.py
@@ -12,3 +12,12 @@ def test_add_user(db):
     db.add_user(1, "Ade")
     assert db.get_user(1) == "Ade"
     
+def test_add_duplicate_user(db):
+    db.add_user(1, "Ade")
+    with pytest.raises(ValueError, match="User already exisits"):
+        db.add_user(1, "Joe")
+        
+def test_delete_user(db):
+    db.add_user(2, "Joe")
+    db.delete_user(2)
+    assert db.get_user(2) is None
